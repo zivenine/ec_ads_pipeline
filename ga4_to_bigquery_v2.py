40546de8f279fb25bee632a32a8ddf343d8c0cfa
Dinesh Ranawaka <ranawaka@gmail.com>
2023-12-18 17:34:58 +0800

    Updated ga4_to_bigquery metrics

--- ga4_to_bigquery_v2.py
+++ ga4_to_bigquery_v2.py
@@ -1,11 +1,19 @@
 from google.oauth2 import service_account
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
-from google.analytics.data_v1beta.types import RunReportRequest
+from google.analytics.data_v1beta.types import (
+    RunReportRequest,
+    DateRange,
+    Dimension,
+    Metric,
+    MetricType,)
 from google.cloud import bigquery
+import credentials
 
 # Path to your service account keys
 ga_service_account_path = 'ecocare-ads-data-e8c8eaeeb5de.json'
 bq_service_account_path = 'ecocare-ads-data-e8c8eaeeb5de.json'
+property_id = credentials.ga4_property_id
+project_id = 'ecocare-ads-data'
 
 # Authenticate with Google Analytics Data API
 ga_credentials = service_account.Credentials.from_service_account_file(ga_service_account_path)
@@ -17,20 +25,44 @@ bq_client = bigquery.Client(credentials=bq_credentials, project=bq_credentials.p
 
 # Define GA4 report request
 report_request = RunReportRequest(
-    property='properties/YOUR_GA4_PROPERTY_ID',
-    dimensions=[{'name': 'city'}],
-    metrics=[{'name': 'activeUsers'}],
-    date_ranges=[{'start_date': '2020-03-31', 'end_date': '2020-04-01'}]
-)
+        property=f"properties/{property_id}",
+        dimensions=[Dimension(name="date"),
+                    Dimension(name="sessionSource"),
+                    Dimension(name="sessionMedium")],
+        metrics=[
+            Metric(name="totalUsers"),
+            Metric(name="activeUsers"),
+            Metric(name="newUsers"),
+            Metric(name="addToCarts"),
+            Metric(name="checkouts"),
+            Metric(name="transactions"),
+            Metric(name="conversions"),
+            Metric(name="userConversionRate"),
+            Metric(name="totalRevenue"),
+        ],
+        date_ranges=[DateRange(start_date="30daysAgo", end_date="today")],
+    )
 
 # Run GA4 report
 response = ga_client.run_report(report_request)
 
+print(response)
+
 # Extract data
 data_for_bq = []
 for row in response.rows:
-    data_for_bq.append({"city": row.dimension_values[0].value, "active_users": int(row.metric_values[0].value)})
-    
+    data_for_bq.append({"date": row.dimension_values[0].value,
+                        "sessionSource": row.dimension_values[1].value,
+                        "sessionMedium": row.dimension_values[2].value,
+                        "totalUsers": int(row.metric_values[0].value),
+                        "active_users": int(row.metric_values[1].value),
+                        "newUsers": int(row.metric_values[2].value),
+                        "addToCarts": int(row.metric_values[3].value),
+                        "checkouts": int(row.metric_values[4].value),
+                        "transactions": float(row.metric_values[5].value),
+                        "conversions": float(row.metric_values[6].value),
+                        "userConversionRate": float(row.metric_values[7].value),
+                        "totalRevenue": float(row.metric_values[8].value)})  
 print(data_for_bq)
 
 # Define BigQuery dataset and table
@@ -38,7 +70,7 @@ dataset_id = 'ecocare-ads-data.ecocare_ads_data'
 table_id = 'ecocare-ads-data.ecocare_ads_data.ecocare_ga4_all_sources'
 
 # Create a BigQuery dataset if not exists
-dataset = bigquery.Dataset(f"{bq_client.project}.{dataset_id}")
+dataset = bigquery.Dataset(f"{dataset_id}")
 dataset.location = "US"
 bq_client.create_dataset(dataset, exists_ok=True)
 
@@ -47,7 +79,7 @@ schema = [
     bigquery.SchemaField("city", "STRING"),
     bigquery.SchemaField("active_users", "INTEGER"),
 ]
-table = bigquery.Table(f"{bq_client.project}.{dataset_id}.{table_id}", schema=schema)
+table = bigquery.Table(f"{table_id}", schema=schema)
 table = bq_client.create_table(table, exists_ok=True)
 
 # Insert data into BigQuery
--- google_ads_to_bigquery.py
+++ google_ads_to_bigquery.py
@@ -16,22 +16,27 @@ def google_ads_to_bigquery():
         google_ads_client = GoogleAdsClient.load_from_storage('google_ads_config.yaml')
 
         # Initialize BigQuery Client
-        credentials = service_account.Credentials.from_service_account_file('ecocare-ads-data-26533bc415de.json')
-        bigquery_client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+        creds = service_account.Credentials.from_service_account_file('ecocare-ads-data-26533bc415de.json')
+        bigquery_client = bigquery.Client(credentials=creds, project=credentials.bq_project_id)
 
         # Define GAQL Query
         query = """
             SELECT campaign.id, campaign.name, metrics.impressions, metrics.clicks, metrics.ctr, metrics.average_cpc,
                 metrics.conversions, metrics.cost_per_conversion, metrics.cost_micros
             FROM campaign
-            WHERE segments.date DURING LAST_90_DAYS
+            WHERE segments.date DURING LAST_30_DAYS
         """
 
         # Fetch Data from Google Ads
-        search_request = google_ads_client.service.google_ads.search(
-            customer_id=credentials.google_ads_customer_id,
-            query=query
-        )
+        # search_request = google_ads_client.service.google_ads.search(
+        #    customer_id=credentials.google_ads_customer_id,
+        #    query=query
+        #)
+        
+        # Fetch Data from Google Ads using Search
+        google_ads_service = google_ads_client.get_service('GoogleAdsService', version='v15')  # Replace 'vX' with the appropriate API version
+        search_request = google_ads_service.search(customer_id=credentials.google_ads_customer_id, query=query)
+
 
         # Prepare Data for BigQuery
         rows_to_insert = []
@@ -50,8 +55,8 @@ def google_ads_to_bigquery():
             rows_to_insert.append(campaign_data)
 
         # Send Data to BigQuery
-        dataset_id = 'google_ads_dataset_id'
-        table_id = 'google_ads_table_id'
+        dataset_id = credentials.google_ads_dataset_id
+        table_id = credentials.google_ads_table_id
         table_ref = bigquery_client.dataset(dataset_id).table(table_id)
         errors = bigquery_client.insert_rows_json(table_ref, rows_to_insert)
 
@@ -76,3 +81,5 @@ def google_ads_to_bigquery():
     except Exception as e:
         # Handle other exceptions
         logging.error(f'An unexpected error occurred: {e}')
+
+google_ads_to_bigquery()
